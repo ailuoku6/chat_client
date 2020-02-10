b585c9c0bec7913d3c6ea4011acd8290bb91b4fa
ailuoku6-blog <18897514992@163.com>
2020-02-10 21:44:03 +0800

    homepage

--- lib/pages/chatPage.dart
+++ lib/pages/chatPage.dart
@@ -1,6 +1,8 @@
 import 'package:chat_client/application.dart';
 import 'package:chat_client/model/Contact.dart';
 import 'package:chat_client/model/Message.dart';
+import 'package:chat_client/socket/socketUtil.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_client/widget/DialogueLeft.dart';
 import 'package:chat_client/widget/DialogueRight.dart';
@@ -19,6 +21,9 @@ class _chatPageState extends State<chatPage> {
 
   List<Message> _messages = new List();
   Map<int,Contact> _contacts = new Map();
+  ScrollController _scrollController = new ScrollController();
+  TextEditingController _textEditingController = new TextEditingController();
+  final double bottomSheetHigh = 80.0;
 
   @override
   void initState() {
@@ -41,6 +46,22 @@ class _chatPageState extends State<chatPage> {
 
   }
 
+  void _sendMsg(){
+    int to = widget._id;
+    String msg = _textEditingController.text;
+    String key = to.toString() + "-" + _messages.length.toString();
+    if(msg=='') return;
+
+    Map<int,Contact> contacts = Application.contacts;
+    contacts[to].msgs.add(new Message(-1,Application.user.id,to,msg,0,new DateTime.now()));
+    Application.contacts = contacts;
+    Application.reflashSubject();
+    socketUtil.sendMsg(to, msg, key);
+
+    _textEditingController.text = '';
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,10 +69,12 @@ class _chatPageState extends State<chatPage> {
         title: (_contacts[widget._id]!=null?Text(_contacts[widget._id].user.nickname+'('+(_contacts[widget._id].online==1?'在线':'离线')+')'):null),
       ),
       body: Container(
+        margin: EdgeInsets.only(bottom: bottomSheetHigh),
         child: RefreshIndicator(
           onRefresh: _initData,
           child: ListView.builder(
             itemCount: _messages.length,
+            controller: _scrollController,
             itemBuilder: (BuildContext context,int index){
 
               Message msg = _messages[index];
@@ -61,15 +84,50 @@ class _chatPageState extends State<chatPage> {
           ),
         ),
       ),
-      bottomNavigationBar: Container(
-        height: 90,
+//      bottomNavigationBar: Container(
+//        height: 90,
+//        color: Colors.black12,
+//        child: TextField(
+//          decoration: InputDecoration(
+//              labelText: "",
+////                        hintText: "用户名或邮箱",
+//          ),
+//        ),
+//      ),
+      bottomSheet: Container(
+        height: bottomSheetHigh,
         color: Colors.black12,
-        child: TextField(
-          decoration: InputDecoration(
-              labelText: "",
-//                        hintText: "用户名或邮箱",
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: TextField(
+                  controller: _textEditingController,
+                  decoration: InputDecoration(
+                    labelText: "",
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    highlightColor: Colors.blue[700],
+                    colorBrightness: Brightness.dark,
+                    splashColor: Colors.grey,
+                    child: Text("发送"),
+                    shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                    onPressed: ()=>_sendMsg(),
+                  ),
+                )
+              )
+            ],
           ),
-        ),
+        )
       ),
     );
   }
