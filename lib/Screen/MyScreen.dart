da008fc61a9879d69f97f960fe7f7ef226ff080c
ailuoku6-blog <18897514992@163.com>
2020-02-12 19:47:16 +0800

    完善

--- lib/Screen/MyScreen.dart
+++ lib/Screen/MyScreen.dart
@@ -12,26 +12,48 @@ class _MyScreenState extends State<MyScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
+      child: Stack(
         children: <Widget>[
           Container(
-            color: Colors.blue,
-            height: 270,
-            child: Stack(
-              children: <Widget>[
-                MyBackground(heightPercentange: 0.5,),
-                Center(
-                  child: CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Colors.blueAccent,
-                      child: Text(Application.user.nickname[0],style: TextStyle(fontSize: 50,fontWeight: FontWeight.w300,color: Colors.white))
+              color: Colors.blue,
+              height: 270,
+              child: Stack(
+                children: <Widget>[
+//                  MyBackground(heightPercentange: 0.5,),
+                  Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        CircleAvatar(
+                            radius: 50,
+                            backgroundColor: Colors.blueAccent,
+                            child: Text(Application.user.nickname[0],style: TextStyle(fontSize: 50,fontWeight: FontWeight.w300,color: Colors.white))
+                        ),
+                        Text(Application.user.nickname,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w300,color: Colors.white)),
+                      ],
+                    )
                   ),
-                ),
+                ],
+              )
+          ),
+          Positioned(
+            top: 220,
+            bottom: 20,
+            left: 8,
+            right: 8,
+            child: Column(
+              children: <Widget>[
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child:
+                  ),
+                )
               ],
             )
-          )
+          ),
         ],
-      ),
+      )
     );
   }
 }
